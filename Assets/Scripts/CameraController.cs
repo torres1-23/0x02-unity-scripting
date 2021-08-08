046f3ed637bbf24e2cbc7ed99ba05dcbaa55e12d
Alejandro Torres <alejo.torres1247@gmail.com>
2021-08-08 17:36:04 -0500

    Add scripts for camera behavior, main camera object

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>Handles main camera class</summary>
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 cameraPos; 
+
+    // Update is called once per frame
+    /// <summary>Updates camera position based on player ball position</summary>
+    void Update()
+    {
+        transform.position = player.transform.position + cameraPos;
+    }
+}
