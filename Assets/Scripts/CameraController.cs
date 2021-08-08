e2016ed4bcb81e520f60534e70f53c38e9bd4fda
Alejandro Torres <alejo.torres1247@gmail.com>
2021-08-08 17:39:35 -0500

    Defined default camera position

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    public Vector3 cameraPos; 
+    public Vector3 cameraPos = new Vector3 (0, 20, 0); 
 
     // Update is called once per frame
     /// <summary>Updates camera position based on player ball position</summary>
